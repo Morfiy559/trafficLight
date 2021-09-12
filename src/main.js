657500665d00058ed9323ad0917be4a6ef9e3b16
Max <maks-berd@mail.ru>
2021-09-12 12:19:46 +0300

    удалил vuex

--- src/main.js
+++ src/main.js
@@ -2,21 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import router from './router/index';
-import Vuex from 'vuex';
-Vue.use(Vuex);
+
 Vue.use(VueRouter);
 Vue.config.productionTip = false
 
-// const store = new Vuex.Store({
-//     state: {
-//             counter : localStorage.counter
-//     },
-//     mutations: {
-//         decrement (state) {
-//             state.counter--
-//         }
-//     }
-// })
 
 new Vue({
     render: h => h(App),
